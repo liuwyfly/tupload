481f21a2f4d339ef564ea37c633ae96cddbae86c
liuweiyin <liuwyfly@hotmail.com>
2019-06-26 18:07:57 +0800

    增加自定义上传路径

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import shutil
 import os
 import json
 
+
 class FileUploadHandler(tornado.web.RequestHandler):
     def get(self):
         self.write('''
@@ -41,10 +42,31 @@ class FileUploadHandler(tornado.web.RequestHandler):
         self.write(json.dumps(ret))
 
 
+class CustomDirUploadHandler(FileUploadHandler):
+    def post(self):
+        ret = {'result': 'OK'}
+        upload_path = '/home/lwylys/workspace/wall/tf'  # 文件的暂存路径
+        file_metas = self.request.files.get('file', None)  # 提取表单中‘name’为‘file’的文件元数据
+
+        if not file_metas:
+            ret['result'] = 'Invalid Args'
+            return ret
+
+        for meta in file_metas:
+            filename = meta['filename']
+            file_path = os.path.join(upload_path, filename)
+
+            with open(file_path, 'wb') as up:
+                up.write(meta['body'])
+                # OR do other thing
+
+        self.write(json.dumps(ret))
+
+
 if __name__ == '__main__':
     app = tornado.web.Application([
         (r'/file', FileUploadHandler),
-        (r'/mydir', FileUploadHandler),
+        (r'/mydir', CustomDirUploadHandler)
     ])
 
     server = tornado.httpserver.HTTPServer(app, max_buffer_size=524288000, max_body_size=524288000)  # 10G
